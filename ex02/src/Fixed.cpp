28b19e0932ac3bae70e459748d91119b7a4d1bd8
Louis Galloux <lgalloux@c2r1p1.42lehavre.fr>
2025-03-22 12:51:44 +0100

    unfinished work, operator overload to fix

---
+++ ex02/src/Fixed.cpp
@@ -0,0 +1,164 @@
+#include "Fixed.hpp"
+#include <iostream>
+#include <cmath>
+
+const int  Fixed::mFrac = 8;
+
+Fixed::Fixed()
+  :mFix(0)
+{
+}
+
+Fixed::Fixed(const int fixed)
+  :mFix(fixed * int(1 << mFrac))
+{
+}
+
+Fixed::Fixed(const float fixed)
+  :mFix(float(round(float(fixed) * float((1 << mFrac)))))
+{
+}
+
+Fixed::Fixed(const Fixed& fixed)
+  :mFix(fixed.mFix)
+{
+}
+
+Fixed& Fixed::operator=(const Fixed& fixed)
+{
+  if (this == &fixed)
+    return (*this);
+  this->setRawBits(fixed.mFix);
+  return (*this);
+}
+
+Fixed::~Fixed()
+{
+}
+
+int  Fixed::getRawBits(void) const
+{
+  std::cout << "getRawBits member function called\n";
+  return (mFix);
+}
+
+void  Fixed::setRawBits(const int raw)
+{
+  mFix = raw;
+}
+
+int  Fixed::toInt(void) const
+{
+  return ((int)mFix / int((1 << mFrac)));
+}
+
+float  Fixed::toFloat(void) const
+{
+  return ((float)mFix / float((1 << mFrac)));
+}
+
+Fixed&  Fixed::min(Fixed& fixed0, Fixed& fixed1)
+{
+  return (fixed0 < fixed1 ? fixed0 : fixed1);
+}
+
+const Fixed&  Fixed::min(const Fixed& fixed0, const Fixed& fixed1)
+{
+  return (fixed0 < fixed1 ? fixed0 : fixed1);
+}
+
+Fixed&  Fixed::max(Fixed& fixed0, Fixed& fixed1)
+{
+  return (fixed0 > fixed1 ? fixed0 : fixed1);
+}
+
+const Fixed&  Fixed::max(const Fixed& fixed0, const Fixed& fixed1)
+{
+  return (fixed0 > fixed1 ? fixed0 : fixed1);
+}
+
+bool  Fixed::operator>(const Fixed& fixed) const
+{
+  return (this->mFix > fixed.mFix ? true : false);
+}
+
+bool  Fixed::operator<(const Fixed& fixed) const
+{
+  return (this->mFix < fixed.mFix ? true : false);
+}
+
+bool  Fixed::operator>=(const Fixed& fixed) const
+{
+  return (this->mFix >= fixed.mFix ? true : false);
+}
+
+bool  Fixed::operator<=(const Fixed& fixed) const
+{
+  return (this->mFix <= fixed.mFix ? true : false);
+}
+
+bool  Fixed::operator==(const Fixed& fixed) const
+{
+  return (this->mFix == fixed.mFix ? true : false);
+}
+
+bool  Fixed::operator!=(const Fixed& fixed) const
+{
+  return (this->mFix != fixed.mFix ? true : false);
+}
+
+Fixed&  Fixed::operator+(const Fixed& fixed)
+{
+  this->mFix = this->toFloat() + fixed.toFloat();
+  return (*this);
+}
+
+Fixed&  Fixed::operator-(const Fixed& fixed)
+{
+  this->mFix = this->toFloat() - fixed.toFloat();
+  return (*this);
+}
+
+Fixed&  Fixed::operator*(const Fixed& fixed)
+{
+  this->mFix = float(this->toFloat() * fixed.toFloat());
+  return (*this);
+}
+
+Fixed&  Fixed::operator/(const Fixed& fixed)
+{
+  this->mFix = this->toFloat() / fixed.toFloat();
+  return (*this);
+}
+
+Fixed&  Fixed::operator++()
+{
+  this->mFix++;
+  return (*this);
+}
+
+Fixed  Fixed::operator++(int)
+{
+  Fixed  temp = *this;
+  this->mFix++;
+  return (temp);
+}
+
+Fixed&  Fixed::operator--()
+{
+  this->mFix--;
+  return (*this);
+}
+
+Fixed  Fixed::operator--(int)
+{
+  Fixed  temp = *this;
+  this->mFix++;
+  return (temp);
+}
+
+std::ostream&  operator<<(std::ostream& out, const Fixed& fixed)
+{
+  out << fixed.toFloat();
+  return (out);
+}
